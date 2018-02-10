374c0664329d90e95e7047b12b2e92e34651f950
subdiox <subdiox@gmail.com>
2018-02-10 23:24:40 +0900

    remove trash

--- 2A/ソフトウェアⅡ/SOFT-01-11-00640195/huffman1/decode.h
+++
@@ -1,6 +0,0 @@
-#ifndef HUFFMAN_DECODE_H
-#define HUFFMAN_DECODE_H
-
-void decode(const char *input_file, const char *output_file);
-
-#endif
--- 2A/ソフトウェアⅡ/SOFT-01-11-00640195/huffman1/encode.h
+++
@@ -1,6 +0,0 @@
-#ifndef HUFFMAN_ENCODE_H
-#define HUFFMAN_ENCODE_H
-
-void encode(const char *input_file, const char *output_file);
-
-#endif
